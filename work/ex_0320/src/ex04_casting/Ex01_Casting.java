d0650d3e3919b4adb9148f4f5c49c949e2f3177d
yeonseo0612 <qnq11@naver.com>
2025-03-20 15:49:28 +0900

    2025_03_20_day02

--- work/ex_0320/src/ex04_casting/Ex01_Casting.java
+++ work/ex_0320/src/ex04_casting/Ex01_Casting.java
@@ -15,6 +15,49 @@ public class Ex01_Casting {
 		System.out.println(num);
 		System.out.println(num2);
 		System.out.println(numChange);
+		System.out.println((int)1.23);
+		
+		//형 변환은 숫자를 담을 수 있는 기본 자료형간에만 가능
+		//정수형, 실수형 자료형 뿐만아니라
+		//문자이면서 동시에 숫자기도한 문자형도 가능하다.
+		
+		//자동형변환(promotion)
+		//서로 다른 자료형간의 대입이나 연산을 할 때 형 변환으로 자료형을 일치시켜야만
+		//하지만 다음과 같은 경우 자바의 컴파일러가 자동으로 형 변환을 해주기 때문에 생략이 가능하다.
+		
+		
+		//작은 자료형에서 큰 자료형으로 변환할 때
+		//정수형
+		//byte -> short -> int -> long
+		//실수형
+		//float(4바이트) -> double(8바이트)
+		
+		//2 정수형이 실수형으로 바뀔때
+		
+		int i = 14;
+		double d = 3.14;
+		d = i;// 자동형 변환
+		
+		System.out.println(d);
+		
+		char c ='a';
+		i = c; //char형에서 int형으로 자동 형변환
+		System.out.println(i);
+		
+		//강제형변환(Demotion)
+		//(원하는 자료형) 데이터 or 변수;
+		
+		//큰쪽에서 작은쪽으로 바꿀때 강제로 바꿔야한다.
+		
+		int x = 10; //4byte
+		byte y = (byte)i; //1byte
+		
+		System.out.println("[int -> byte ] x의 값" + x +" y의 값 :" + y);
+		
+		//int -> byte 강제 형변환 (값이 큰 경우)
+		int j = 1000;
+		byte k = (byte)j;
+		System.out.println("[int -> byte ] j의 값" + j +" k의 값 :" + k);
 	}
 
 }
