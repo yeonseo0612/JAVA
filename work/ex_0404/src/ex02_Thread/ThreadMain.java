b47e4c21c6829a259232ebade8c051687b1707cb
yeonseo0612 <qnq11@naver.com>
2025-04-04 18:21:17 +0900

    2025_04_04_day12

--- work/ex_0404/src/ex02_Thread/ThreadMain.java
+++ work/ex_0404/src/ex02_Thread/ThreadMain.java
@@ -16,8 +16,8 @@ public class ThreadMain {
 		
 		Thread t = new Thread(t2);
 		t.start();
-		//main도 하나으 ㅣ스레드이다
-		System.out.println("현재 실행되고 있는 스레드의 이름: " + Thread.currentThread().getName());
+		//main도 하나의스레드이다
+		 		System.out.println("현재 실행되고 있는 스레드의 이름: " + Thread.currentThread().getName());
 		System.out.println("현재 실행되고 있는 스레드의 상태: " + Thread.currentThread().getState());
 		System.out.println("현재 실행되고 있는 스레드의 우선순위: " + Thread.currentThread().getPriority());
 		
