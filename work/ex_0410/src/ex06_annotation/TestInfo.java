662c954e97c17298c49ff4ab5022db4e1599c367
yeonseo0612 <qnq11@naver.com>
2025-04-10 18:07:30 +0900

    2025_04_10_day16

--- work/ex_0410/src/ex06_annotation/TestInfo.java
+++ work/ex_0410/src/ex06_annotation/TestInfo.java
@@ -1,7 +1,15 @@
 package ex06_annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+//선언 키워드는 @inteface
+//어노테이션을 적용할 타겟
+@Target(ElementType.TYPE)
 //선언 키워드는 @interface
+@Retention(RetentionPolicy.RUNTIME)
 public @interface TestInfo {
 	//어노테이션의 속성은 메서드 형태로 정의한다
 	
@@ -14,6 +22,12 @@ public @interface TestInfo {
 	//배열
 	
 	//속성에 기본값을 지정할 수 있다.
+	//예외를 던질 수 없다.
+	//String value() throw Exception;
+	
+	//속성은 매개변수를 가질 수 없다.
+	String name(String input);
+	
 	
 	String value() default "홍길동"; //추상메서드로 선언해야한다.
 	
