e71045d4295944aa03b98cb14d8a409cfa4c6545
yeonseo0612 <qnq11@naver.com>
2025-04-11 18:10:09 +0900

    202504_11_day17

--- work/ex_0411/src/ec02_db/PersonMain.java
+++ work/ex_0411/src/ec02_db/PersonMain.java
@@ -6,14 +6,12 @@ public class PersonMain {
 	public static void main(String[] args) {
 		PersonMethod pm = new PersonMethod();
 		List<Person> persons = pm.fine();
-		
-		for(Person p : persons) {
-			System.out.println("번호 : "+p.getIdx()+" 이름 : " + p.getName() + " 나이 : " + p.getAge());
+
+		for (Person p : persons) {
+			System.out.println("번호 : " + p.getIdx() + " 이름 : " + p.getName() + " 나이 : " + p.getAge());
 		}
-		
+
 //		pm.addPerson(4, "구연서", 25);
 
-		
-		
 	}
 }
--- work/ex_0411/src/ec02_db/PersonMethod.java
+++ work/ex_0411/src/ec02_db/PersonMethod.java
@@ -8,102 +8,98 @@ import java.util.List;
 
 public class PersonMethod {
 
-	public List<Person> fine(){
+	public List<Person> fine() {
 		List<Person> list = new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql = "SELECT * FROM person";
-		
+
 		try {
 			conn = DBUtil.getConnection();
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Person person = new Person();
 				int idx = rs.getInt("idx");
 				int age = rs.getInt("age");
 				String name = rs.getString("name");
-				
+
 				person.setAge(age);
 				person.setIdx(idx);
 				person.setName(name);
-				
+
 				list.add(person);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			  System.out.println("오류 발생: " + e.getMessage());
-	            e.printStackTrace();
-		}finally {
+			System.out.println("오류 발생: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					pstmt.close();
 				}
 			} catch (Exception e2) {
 				// TODO: handle exception
-				  System.out.println("자원 해제 중 오류 발생: " + e2.getMessage());
+				System.out.println("자원 해제 중 오류 발생: " + e2.getMessage());
 			}
 		}
 		return list;
 	}
-	
+
 	public int addPerson(int idx_in, String name_in, int age_in) {
 		int result = 0;
-		
+
 		List<Person> list = new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		String sql = "insert into person(idx,name,age)values(?,?,?)";
-		
+
 		try {
 			conn = DBUtil.getConnection();
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setInt(1, idx_in);
 			pstmt.setString(2, name_in);
 			pstmt.setInt(3, age_in);
 			int res = pstmt.executeUpdate();
-			
 
-		    if (res > 0) {
-		        System.out.println("삽입 성공: " + res);
-		    } else {
-		        System.out.println("삽입 실패");
-		    }
+			if (res > 0) {
+				System.out.println("삽입 성공: " + res);
+			} else {
+				System.out.println("삽입 실패");
+			}
 
-			
-			
 		} catch (Exception e) {
 			// TODO: handle exception
-			  System.out.println("오류 발생: " + e.getMessage());
-	            e.printStackTrace();
-		}finally {
+			System.out.println("오류 발생: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					pstmt.close();
 				}
 			} catch (Exception e2) {
 				// TODO: handle exception
-				  System.out.println("자원 해제 중 오류 발생: " + e2.getMessage());
+				System.out.println("자원 해제 중 오류 발생: " + e2.getMessage());
 			}
 		}
-		
-		
+
 		return result;
 	}
-	
+
 }
